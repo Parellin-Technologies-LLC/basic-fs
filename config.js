b7a6da3b383f441177e4c2bc984d23bacfcc4391
iskore <iSkore@users.noreply.github.com>
2018-01-05 15:30:46 -0500

    Refactored get, post, and delete methods to allow deep files and dirs

--- config.js
+++ config.js
@@ -1,15 +1,26 @@
 'use strict';
 
-const { version } = require( './package.json' );
+const
+	{ version } = require( './package.json' ),
+	{ join }    = require( 'path' );
 
 module.exports = {
 	version,
-    cwd: process.cwd(),
-    publicDirectory: 'public',
-    port: 3000,
-    ping: '/ping',
-    kill: '/kill',
-    docs: '/docs',
-    path: '/files',
-    proxy: '/files/:name'
+	cwd: process.cwd(),
+	dataDirectory: 'data/',
+	dotfiles: 'allow',
+	timeout: 20000,
+	maximumURISize: 1600,
+	maximumHeaderSize: 4000,
+	maximumPayloadSize: 53687091200,
+	minimumHTTPVersion: 1.0,
+	port: 3000,
+	ping: '/ping',
+	kill: '/kill',
+	docs: '/docs',
+	data: [ '/data', '/data/*' ]
 };
+
+// TODO: create a "download" function to zip up files and download
+
+module.exports.root = join( module.exports.cwd, module.exports.dataDirectory );
--- index.js
+++ index.js
@@ -1,167 +1,54 @@
 'use strict';
 
 const
-    config     = require( './config' ),
-    {
-        getFile,
-        listFiles,
-        saveFile,
-        deleteFile,
-    }          = require( './lib/fswrapper' ),
-    {
-        ensureDir
-    }          = require( 'fs-extra' ),
-    path       = require( 'path' ),
-    crypto     = require( 'crypto' ),
-    Response   = require( 'http-response-class' ),
-    express    = require( 'express' ),
-    formidable = require( 'formidable' ),
-    op         = require( 'openport' ),
-    lanIP      = require( './lib/lanIP' ),
-    app        = express(),
-    catchAll   = res => {
-        res.setHeader( 'Content-Type', 'application/json' );
-        res.json( config );
-    },
-    find       = opt => new Promise(
-        ( res, rej ) => op.find( opt,
-            ( e, p ) => e ? rej( e ) : res( p )
-        )
-    );
+	config        = require( './config' ),
+	path          = require( 'path' ),
+	Response      = require( 'http-response-class' ),
+	express       = require( 'express' ),
+	op            = require( 'openport' ),
+	{ ensureDir } = require( './lib/fswrapper' ),
+	uploadData    = require( './lib/uploadData' ),
+	getData       = require( './lib/getData' ),
+	deleteData    = require( './lib/deleteData' ),
+	ping          = require( './lib/ping' ),
+	kill          = require( './lib/kill' ),
+	inspection    = require( './lib/inspection' ),
+	lanIP         = require( './lib/lanIP' ),
+	app           = express(),
+	find          = opt => new Promise(
+		( res, rej ) => op.find( opt,
+			( e, p ) => e ? rej( e ) : res( p )
+		)
+	);
 
-app.all( config.ping, ( req, res ) => {
-    res.setHeader( 'Content-Type', 'application/json' );
-    res.json( new Response( 200, 'pong' ) );
-} );
+config.root = path.join( config.cwd, config.dataDirectory );
 
-app.all( config.kill, ( req, res ) => ( res.send( 'closed server' ), process.exit() ) );
+app.use( inspection() );
 
-app.all( config.path, ( req, res ) => {
-    res.setHeader( 'Content-Type', 'application/json' );
+app.all( config.ping, ping );
+app.all( config.kill, kill );
+app.get( config.data, getData );
+app.post( config.data, uploadData );
 
-    const fpath = path.join( config.cwd, config.publicDirectory );
-    let statusCode = 0, message = '';
+app.delete( config.data, deleteData );
 
-    listFiles( fpath )
-        .then( r => ( statusCode = 200, message = r ) )
-        .catch( e => ( statusCode = 500, message = e ) )
-        .then( () => new Response( statusCode, message ) )
-        .then( r => ( res.status( r.statusCode ), r ) )
-        .then( r => res.json( r ) )
-        .catch( console.error );
-} );
+app.all( config.docs,
+	( req, res ) => {
+		res.setHeader( 'Content-Type', 'application/json' );
+		res.send( new Response( 200, config ).toString() );
+	}
+);
 
-app.get( config.proxy, ( req, res ) => {
-    const
-        options = {
-            root: path.join( config.cwd, config.publicDirectory ),
-            dotfiles: 'allow',
-            headers: {
-                'x-timestamp': Date.now(),
-                'x-sent': true
-            }
-        },
-        fname   = req.params.name;
-
-    res.sendFile( fname, options );
-} );
-
-app.post( config.proxy, ( req, res ) => {
-    const form = new formidable.IncomingForm();
-    let statusCode = 0, message = '';
-
-    form.parse( req, ( err, fields, files ) => {
-        if( typeof fields === 'object' && !Object.keys( files ).length ) {
-            const
-                fname = req.params.name || `${crypto.randomBytes( 32 ).toString( 'hex' )}.json`,
-                fpath = path.join( config.cwd, 'public', fname );
-
-            saveFile( fpath, JSON.stringify( fields ) )
-                .then( () => ( statusCode = 200, message = fpath ) )
-                .catch( e => ( statusCode = 500, message = e ) )
-                .then( () => new Response( statusCode, message ) )
-                .then( r => ( res.status( r.statusCode ), r ) )
-                .then( r => res.json( r ) )
-                .catch( console.error );
-        } else {
-            console.log( fields, files );
-
-            const
-                ops = Object.keys( files ).reduce(
-                    ( r, i ) => {
-                        const fname = path.join( config.cwd, config.publicDirectory, i );
-                        r.push(
-                            getFile( files[ i ].path )
-                                .then( data => saveFile( fname, data ) )
-                                .then( () => ( {
-                                    delete: files[ i ].path,
-                                    save: fname
-                                } ) )
-                                .catch( e => e )
-                        );
-                        return r;
-                    }, []
-                );
-
-            Promise.all( ops )
-                .then(
-                    items => items.reduce(
-                        ( r, i ) => (
-                            r.push(
-                                deleteFile( i.delete )
-                                    .then( () => i.save )
-                                    .catch( () => false )
-                            ),
-                                r
-                        ), []
-                    )
-                )
-                .then( items => Promise.all( items ) )
-                .then(
-                    items => items.indexOf( false ) === -1 ? items : false
-                )
-                .then(
-                    r => r ?
-                        ( statusCode = 201, message = r ) :
-                        ( statusCode = 500, message = 'Error' )
-                )
-                .catch( e => ( statusCode = 500, message = e ) )
-                .then( () => new Response( statusCode, message ) )
-                .then( r => ( res.status( r.statusCode ), r ) )
-                .then( r => res.json( r ) )
-                .catch( console.error );
-        }
-    } );
-} );
-
-app.delete( config.proxy, ( req, res ) => {
-    const fname = path.join( config.cwd, 'public', req.params.name );
-    let statusCode = 0, message = '';
-
-    deleteFile( fname )
-        .then( () => ( statusCode = 200, message = 'Deleted' ) )
-        .catch(
-            e => e.code === 'ENOENT' ?
-                ( statusCode = 404, message = 'File Not Found' ) :
-                ( statusCode = 500, message = e )
-        )
-        .then( () => new Response( statusCode, message ) )
-        .then( r => ( res.status( r.statusCode ), r ) )
-        .then( r => res.json( r ) )
-        .catch( console.error );
-} );
-
-app.all( config.docs, ( req, res ) => catchAll( res ) );
 app.all( '*', ( req, res ) => catchAll( res ) );
 
 
-ensureDir( config.publicDirectory )
-    .then(
-        () => find( { startingPort: config.port, endingPort: config.port + 50 } )
-    )
-    .then(
-        port => app.listen(
-            port,
-            () => console.log( `BasicFS v${config.version} running on ${lanIP}:${port}` )
-        )
-    );
+ensureDir( config.dataDirectory )
+	.then(
+		() => find( { startingPort: config.port, endingPort: config.port + 50 } )
+	)
+	.then(
+		port => app.listen(
+			port,
+			() => console.log( `BasicFS v${config.version} running on ${lanIP}:${port}` )
+		)
+	);
